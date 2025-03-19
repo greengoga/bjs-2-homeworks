f43eeb8e88c21ae17c0bea040bc7d6d5d69c2459
O.Gogol <o.gogol91@gmail.com>
2025-03-19 19:10:55 +0000

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+
+	if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthlyPercent = (percent / 100) / 12;
+	let mortgageBody = amount - contribution;
+	let monthlyPayment = mortgageBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+	let totalAmount = monthlyPayment * countMonths;
+	return Number(totalAmount.toFixed(2));
 }
